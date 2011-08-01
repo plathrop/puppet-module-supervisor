fe3b47f2ce6bfb9d9f49ad17fee5aaaef8e5dd64
Paul Lathrop <paul@simplegeo.com>
2011-08-01 11:50:42 -0700

    Merge in portions of patch from @jorgecardona. Closes #3

--- manifests/init.pp
+++ manifests/init.pp
@@ -32,7 +32,8 @@ class supervisor {
       require => Package['supervisor'];
     $supervisor_conf_file:
       content => template('supervisor/supervisord.conf.erb'),
-      require => Package['supervisor'];
+      require => Package['supervisor'],
+      notify  => Service[$supervisor_system_service];
     '/etc/logrotate.d/supervisor':
       source => 'puppet:///modules/supervisor/logrotate',
       require => Package['supervisor'];
