83929ddf368b10ed02028084f0f7441c53fb5b22
Mattias Geniar <m@ttias.be>
2013-02-15 17:29:22 +0100

    Style and documentation improvements

--- manifests/params.pp
+++ manifests/params.pp
@@ -1,16 +1,16 @@
 class supervisor::params {
   case $::operatingsystem {
     'ubuntu','debian': {
-      $conf_file = '/etc/supervisor/supervisord.conf'
-      $conf_dir = '/etc/supervisor'
+      $conf_file      = '/etc/supervisor/supervisord.conf'
+      $conf_dir       = '/etc/supervisor'
       $system_service = 'supervisor'
-      $package = 'supervisor'
+      $package        = 'supervisor'
     }
     'centos','fedora','redhat': {
-      $conf_file = '/etc/supervisord.conf'
-      $conf_dir = '/etc/supervisord.d'
+      $conf_file      = '/etc/supervisord.conf'
+      $conf_dir       = '/etc/supervisord.d'
       $system_service = 'supervisord'
-      $package = 'supervisor'
+      $package        = 'supervisor'
     }
     default: {
       fail("Unsupported platform: ${::operatingsystem}")
