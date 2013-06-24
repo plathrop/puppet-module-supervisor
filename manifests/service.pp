a627ae11b36b3e8c7b5008132eb1b30443b55d87
Jake Maul <nmaul@mozilla.com>
2013-06-24 14:33:21 -0700

    make start/stop/restart return sensible error codes too, like status

--- manifests/service.pp
+++ manifests/service.pp
@@ -90,10 +90,10 @@ define supervisor::service (
   service { "supervisor::${name}":
     ensure   => $service_ensure,
     provider => base,
-    restart  => "/usr/bin/supervisorctl restart ${process_name}",
-    start    => "/usr/bin/supervisorctl start ${process_name}",
+    restart  => "/usr/bin/supervisorctl restart ${process_name} | awk '/^${name}[: ]/{print \$2}' | grep -Pzo '^stopped\nstarted$'",
+    start    => "/usr/bin/supervisorctl start ${process_name} | awk '/^${name}[: ]/{print \$2}' | grep '^started$'",
     status   => "/usr/bin/supervisorctl status | awk '/^${name}[: ]/{print \$2}' | grep '^RUNNING$'",
-    stop     => "/usr/bin/supervisorctl stop ${process_name}",
+    stop     => "/usr/bin/supervisorctl stop ${process_name} | awk '/^${name}[: ]/{print \$2}' | grep '^stopped$'",
     require  => [Class['supervisor::update'], File["${supervisor::params::conf_dir}/${name}.ini"]],
   }
 }
