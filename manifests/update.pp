d5c0fbd28f46eb4fba922fc98b45d693225987e7
Andrew Hemming <drewhemm@users.noreply.github.com>
2015-09-17 14:25:15 +0100

    Corrected socket test
    Should be testing for a Unix socket, not a regular file

--- manifests/update.pp
+++ manifests/update.pp
@@ -2,7 +2,7 @@ class supervisor::update {
   exec { 'supervisor::update':
     command     => '/usr/bin/supervisorctl update',
     logoutput   => on_failure,
-    onlyif      => "/usr/bin/env test -f ${supervisor::unix_server_file}",
+    onlyif      => "/usr/bin/env test -S ${supervisor::unix_server_file}",
     refreshonly => true,
     require     => Service[$supervisor::params::system_service],
   }
